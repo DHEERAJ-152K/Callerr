43e93e4bd018dd3cd0f1a9ce0d1f6a43810812e4
Dheeraj K P <dheerudheeraj033@gmail.com>
2024-04-18 13:25:03 +0530

    initial commit

---
+++ src/Home.js
@@ -0,0 +1,14 @@
+function Home(){
+    return(
+        <div>
+            <h1>CALLERR..</h1>
+            <p><b>One on One Instant Video Calling Service.</b></p>
+
+            <button>Host +</button>
+            <input placeholder="join via link"></input>
+            <button>join</button>
+        </div>
+    );
+}
+
+export default Home;
